b20af1ae811a157501d63b69b92378162213ef0f
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2025-06-19 17:45:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function reveal() {
-  const card = document.getElementById('entry-card');
+  const entry = document.getElementById('entry-card');
   const port = document.getElementById('portfolio');
-  card.classList.add('opacity-0', 'scale-95');
+  entry.classList.add('opacity-0', 'scale-90');
+
   setTimeout(() => {
-    card.remove();
-    document.body.classList.remove('flex','items-center','justify-center');
+    entry.style.display = "none";
     port.classList.remove('hidden');
   }, 500);
 }
